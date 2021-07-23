fd866faebb98d95ae8bb3869b5ceb3464188a9ae
Sandeep kumar Reddy <55143627+ysandeepkumarreddy@users.noreply.github.com>
2021-07-23 10:02:09 +0530

    commit

--- Mini Projects/Game Of Cards.py
+++ Mini Projects/Game Of Cards.py
@@ -7,148 +7,135 @@ values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eig
           'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}  # Create dictionary of values for each rank
 
 
-class Card:  # Card class
-    def __init__(self, suit, rank):  # Card constructor
-        self.suit = suit  # suit is a string
-        self.rank = rank  # rank is an integer
-        self.value = values[rank]  # value is an integer
+# Create a class for the cards
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit  # create a suit attribute
+        self.rank = rank  # create a rank attribute
+        self.value = values[rank]  # create a value attribute for the cards
 
-    def __str__(self):  # string representation of a Card
-        return self.rank + ' of ' + self.suit  # returns a string
+# create a str method for the cards
+    def __str__(self):
+        # return the rank of the card and the suit
+        return self.rank + ' of ' + self.suit
+
+# Create a class for the deck of cards
 
 
-class Deck:  # Deck class
-    def __init__(self):  # Deck constructor
-        self.all_cards = []  # Create empty list for cards
-        for suit in suits:  # For each suits
+class Deck:
+    def __init__(self):  # create a deck attribute
+        self.all_cards = []  # create an empty list for the deck
+        for suit in suits:  # loop through the suits
             for rank in ranks:
-                create_card = Card(suit, rank)  # Creates a card
-                # Add card to list of all cards
+                create_card = Card(suit, rank)  # create a card class
+                # append the card to the deck
                 self.all_cards.append(create_card)
 
-    def shuffle(self):  # Shuffle the deck
-        random.shuffle(self.all_cards)  # Shuffle the list of cards
+    def shuffle(self):  # create a shuffle method
+        random.shuffle(self.all_cards)  # shuffle the deck
 
-    def dealone(self):
-        return self.all_cards.pop()  # Deal the top card from the deck
+    def deal_one(self):
+        # deal a card by popping the last card in the deck
+        return self.all_cards.pop()
 
 
+# Create a class for the Player of cards
 class Player:
     def __init__(self, name):
-        self.name = name
-        # A new player has no cards
-        self.all_cards = []  # list of cards in player's hand
+        self.name = name  # create a name attribute
+        self.all_cards = []  # create an empty list for the player
 
     def remove_one(self):
-        # Note we remove one card from the list of all_cards
-        # We state 0 to remove from the "top" of the deck
-        # We'll imagine index -1 as the bottom of the deck
-        return self.all_cards.pop(0)  # return the last card in the list
+        # remove a card from the player's hand
+        return self.all_cards.pop(0)
 
-    def add_cards(self, new_cards):
-        if type(new_cards) == type([]):
+    def add_cards(self, new_cards):  # add a card to the player's hand
+        if type(new_cards) == type([]):  # check if the new_cards is a list
+            # add the new_cards to the player's hand
             self.all_cards.extend(new_cards)
         else:
+            # add the new_cards to the player's hand
             self.all_cards.append(new_cards)
 
-    def __str__(self):
+    def __str__(self):  # create a str method for the player
+        # return the number of cards in the player's hand
         return f'Player {self.name} has {len(self.all_cards)} cards.'
 
 
-player_one = Player("One")  # Create a player1
-player_two = Player("Two")  # Create a player2
+player_one = Player('Player One')
+player_two = Player('Player Two')
 
-new_deck = Deck()  # Create a deck
-new_deck.shuffle()  # Shuffle the deck
+new_deck = Deck()
+new_deck.shuffle()
 
+for x in range(26):  # loop through the deck 26 times
+    player_one.add_cards(new_deck.deal_one())  # deal a card to the player
+    player_two.add_cards(new_deck.deal_one())  # deal a card to the player
 
-for x in range(26):  # Deal 26 cards to each player
-    player_one.add_cards(new_deck.dealone())  # Deal card to player1
-    player_two.add_cards(new_deck.dealone())  # Deal card to player2
+game_on = True  # create a game_on variable
+round_number = 0  # create a round_number variable
+while game_on:  # loop through the game
 
-game_on = True  # Game is on
+    round_number += 1  # increase the round number by one
+    print(f'Round {round_number}')  # print the round number
 
-round_num = 0  # Round number
-while game_on:  # While game is on
+    if len(player_one.all_cards) == 0:  # check if the player has any cards
+        # print that the player has no cards left
+        print('Player One has no cards left')
+        print('Player Two Won!!')  # print that player two has won the game
+        game_on = False  # set game_on to False
+        break  # break the loop
+    if len(player_two.all_cards) == 0:  # check if the player has any cards
+        # print that the player has no cards left
+        print('Player Two has no cards left')
+        print('Player One has Won!!')  # print that player one has won the game
+        game_on = False  # set game_on to False
+        break  # break the loop
 
-    round_num += 1  # Increment round number
-    print(f"Round {round_num}")  # Print round number
+    # otherwise, the game continues
 
-    # Check to see if a player is out of cards:
-    if len(player_one.all_cards) == 0:
-        print("Player One out of cards! Game Over")  # Player 1 is out of cards
-        print("Player Two Wins!")   # Player 2 wins
-        game_on = False  # Game is over
-        break   # Exit the loop
-
-    if len(player_two.all_cards) == 0:
-        print("Player Two out of cards! Game Over")  # Player 2 is out of cards
-        print("Player One Wins!")   # Player 1 wins
-        game_on = False  # Game is over
-        break   # Exit the loop
-
-    # Otherwise, the game is still on!
-
-    # Start a new round and reset current cards "on the table"
-    player_one_cards = []   # List of cards in player 1's hand
-    # Add top card from player 1's hand to table
+    player_one_cards = []  # create a list for player one's cards
+    # remove a card from the player's hand
     player_one_cards.append(player_one.remove_one())
-
-    player_two_cards = []   # List of cards in player 2's hand
-    # Add top card from player 2's hand to table
+    player_two_cards = []  # create a list for player two's cards
+    # remove a card from the player's hand
     player_two_cards.append(player_two.remove_one())
 
-    at_war = True   # Set at_war to True
-
-    while at_war:   # While at war, keep dealing cards
-
-        # If player 1's card is higher than player 2's card
+    at_war = True  # create a at_war variable
+    while at_war:  # loop through the game
         if player_one_cards[-1].value > player_two_cards[-1].value:
-
-            # Player One gets the cards
-            # Add player 1's cards to player 1's hand
+            # add the card to the player one's hand
             player_one.add_cards(player_one_cards)
-            # Add player 2's cards to player 1's hand
+            # add the card to the player two's hand
             player_one.add_cards(player_two_cards)
 
-            # No Longer at "war" , time for next round
-            at_war = False  # Set at_war to False
+            at_war = False  # set at_war to False
 
-        # Player Two Has higher Card
+        # check if the player one's card is greater than the player two's card
         elif player_one_cards[-1].value < player_two_cards[-1].value:
-
-            # Player Two gets the cards
-            player_two.add_cards(player_one_cards)
+            # add the player two's card to the player two's hand
             player_two.add_cards(player_two_cards)
+            # Player Two gets Player One's card
+            player_one.add_cards(player_two_cards)
 
-            # No Longer at "war" , time for next round
-            at_war = False
+            at_war = False  # set at_war to False
 
         else:
-            print('WAR!')
-            # This occurs when the cards are equal.
-            # We'll grab another card each and continue the current war.
+            print('War!!')
 
-            # First check to see if player has enough cards
-
-            # Check to see if a player is out of cards:
-            if len(player_one.all_cards) < 3:
-                # Player 1 is out of cards
-                print("Player One unable to play war! Game Over at War")
-                print("Player Two Wins! Player One Loses!")  # Player 2 wins
-                game_on = False  # Game is over
-                break   # Exit the loop
-
-            elif len(player_two.all_cards) < 3:  # Check to see if a player is out of cards
-                # Player 2 is out of cards
-                print("Player Two unable to play war! Game Over at War")
-                print("Player One Wins! Player One Loses!")  # Player 1 wins
-                game_on = False  # Game is over
-                break       # Exit the loop
-            # Otherwise, we're still at war, so we'll add the next cards
+            if len(player_one.all_cards) < 3:  # check if the player has less than 3 cards
+                # print that the player has no cards left
+                print('Player One has no cards left')
+                # print that player two has won the game
+                print('Player Two Won!!')
+                game_on = False  # set game_on to False
+                break  # break the loop
+            elif len(player_two.all_cards) < 3:
+                print('Player Two has no cards left')
+                print('Player One has Won!!')
+                game_on = False  # set game_on to False
+                break  # break the loop
             else:
-                for num in range(3):  # Loop thrice
-                    # Add player 1's cards to table
+                for num in range(3):
                     player_one_cards.append(player_one.remove_one())
-                    # Add player 2's cards to table
                     player_two_cards.append(player_two.remove_one())
